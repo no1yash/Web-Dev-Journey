2c730dfe23175d7eb570076b642931c55eb8a0b6
Yeshwantha S <240023556+no1yash@users.noreply.github.com>
2026-01-28 21:55:47 +0530

    Add files via upload

---
+++ day 18/script.js
@@ -0,0 +1,12 @@
+const userName = "Yash";
+let age = 48;
+
+if (age < 18) {
+    console.log(userName + " is a child.");
+} else if (age >= 60) {
+    console.log(userName + " is a senior citizen.");
+} else {
+    console.log(userName + " is an adult.");
+}
+
+
