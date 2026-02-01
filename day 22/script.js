955ea6dc631f0a03cb955ad9c8626eeee2fa4243
Yeshwantha S <240023556+no1yash@users.noreply.github.com>
2026-02-01 22:12:36 +0530

    Add files via upload

---
+++ day 22/script.js
@@ -0,0 +1,11 @@
+function changeBg(animal){
+    if(animal === "dog"){
+        document.body.style.backgroundColor = "lightblue";
+    }
+    else if(animal === "cat"){
+        document.body.style.backgroundColor = "pink";
+    }
+    else if(animal === "fish"){
+        document.body.style.backgroundColor = "lightyellow";
+    }
+}
